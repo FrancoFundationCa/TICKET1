7415ff61ca25270e410eaf4e6b6f330c6b40aae8
YonathanFranco <35731118+YonathanFranco@users.noreply.github.com>
2021-07-10 04:41:35 -0400

    Create SingUp.php

---
+++ SingUp.php
@@ -0,0 +1,42 @@
+<?PHP
+
+include(conexión.php);
+
+    function insertarClientes( $connection, $nombres, $apellidos, $numbercard, $email, $tlf, $comentarios )
+
+    {
+
+        $sql = "INSERT INTO participantes VALUES(".nombres.", ".apellidos.", ".numbercard.", ".email.", ".tlf.", ".comentarios." , "..")";
+
+        // Ejecutamos la consulta (se devolverá true o false):
+
+       return pg_query ($connection, $sql);
+
+       
+
+     $result = pg_query($connection, $sql);
+
+       if (!$result) {
+
+    '<script> alert("error intenté de nuevo");
+
+    window.history.go.(-1);
+
+    </script>'
+
+    
+
+       }else {
+
+'<script> alert("listo todo echo");
+
+window.history.go.(-1);
+
+</script>'
+
+  pg_query_close($connection);
+
+};
+
+?>
+
