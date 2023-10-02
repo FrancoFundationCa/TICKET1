c71b3f5467d79481e7970d77742962ccfe79002a
YonathanFranco <35731118+YonathanFranco@users.noreply.github.com>
2023-10-02 14:18:55 -0400

    Update SingUp.php

--- SingUp.php
+++ SingUp.php
@@ -2,6 +2,32 @@
 
 include(conexion.php);
 
+function connect_postgreSQl( host, user, password)
+
+    {
+
+         $connection = pg_connect( " host='files.000webhost.com' dbname=d2277cfqe7bpu7 Port='21' user='laboratoriesfranco' password='Hello.12345' "
+
+                               ) or die( "Error al conectar: ".pg_last_error() );
+
+      
+
+      if($connection){
+
+    echo"bien está guardado",
+
+}else {
+
+    echo"error al guardar";
+
+}
+
+      
+
+        return $connection;
+
+    }
+
     function insertarClientes( $connection, $nombres, $apellidos, $numbercard, $email, $tlf, $comentarios )
 
     {
