73130ec3f1da346465d3df883a24f770527ed1f1
YonathanFranco <35731118+YonathanFranco@users.noreply.github.com>
2023-10-02 13:58:08 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -4,7 +4,7 @@ function connect_postgreSQl( host, dbname user, password)
 
     {
 
-         $connection = pg_connect( " host='ec2-52-5-1-20.compute-1.amazonaws.com' dbname=d2277cfqe7bpu7 Port=5432 user=bwopcndfxoaqai password=fd76e1ba2aa84228cce2e924614001aadd453c9f6eb6fa76d49ec13cd2810a94 "
+         $connection = pg_connect( " host='files.000webhost.com' dbname=d2277cfqe7bpu7 Port=21 user=laboratoriesfranco password=Hello.12345 "
 
                                ) or die( "Error al conectar: ".pg_last_error() );
 
