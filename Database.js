54cc7302c08cb2ec4fb866e0d9f25d5eaea7aa80
YonathanFranco <35731118+YonathanFranco@users.noreply.github.com>
2023-10-02 12:48:53 -0400

    Update Database.js

--- Database.js
+++ Database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+  user: "id21342744_yf",
+  password: "hello.12345"
 });
 
 con.connect(function(err) {
