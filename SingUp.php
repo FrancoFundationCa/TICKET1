fc533e8bf51836b5982cadb5e591f3d1ad0e4f79
YonathanFranco <35731118+YonathanFranco@users.noreply.github.com>
2023-10-28 08:43:33 -0400

    Update SingUp.php

--- SingUp.php
+++ SingUp.php
@@ -2,16 +2,6 @@
 
 include(conexion.php);
 
-function connect_postgreSQl( host, user, password)
-
-    {
-
-         $connection = pg_connect( " host='files.000webhost.com' dbname=d2277cfqe7bpu7 Port='21' user='laboratoriesfranco' password='Hello.12345' "
-
-                               ) or die( "Error al conectar: ".pg_last_error() );
-
-      
-
       if($connection){
 
     echo"bien está guardado",
